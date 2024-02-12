3c00480ad4576a2733fdb18e9bf87670c76e7ce2
Hernan Gutierrez <hernangut96@gmail.com>
2024-02-11 21:34:57 -0300

    update script

--- OnlineSoccerManagerBot.py
+++ OnlineSoccerManagerBot.py
@@ -1,35 +1,37 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+import os
 import time
 
-service = FirefoxService(executable_path=GeckoDriverManager().install())
+options = webdriver.FirefoxOptions()
+driver = webdriver.Firefox(options=options)
 
-driver = webdriver.Firefox(service=service)
 
 def login(user,password):
-    driver.get('https://en.onlinesoccermanager.com/Dashboard')
-    time.sleep(10)
-    driver.find_element("xpath",'/html/body/div[2]/div[2]/div/div/div[2]/div[3]/div/button').click()
+    driver.get('https://en.onlinesoccermanager.com')
     time.sleep(5)
-    driver.find_element("xpath",'/html/body/div[2]/div[1]/div[3]/div[4]/div[2]/button').click()
+    driver.find_element('css selector', '.btn-new').click()
     time.sleep(5)
-    driver.find_element("xpath",'//*[@id="manager-name"]').send_keys(user)
-    driver.find_element("xpath",'//*[@id="password"]').send_keys(password)
-    driver.find_element("xpath",'//*[@id="login"]').click()
+    driver.find_element('css selector', '.btn-alternative').click()
+    time.sleep(5)
+    driver.find_element("css selector",'#manager-name').send_keys(user)
+    driver.find_element("css selector",'#password').send_keys(password)
+    driver.find_element("css selector",'#login').click()
     time.sleep(15)
 
 def getBusinessTokens():
+    print('Negocios')
+    time.sleep(5)
+    driver.find_element('css selector', 'li.dropdown:nth-child(3) > a:nth-child(1)').click()
+    time.sleep(5)
+    driver.find_element('css selector', 'div.product-free:nth-child(2)').click()
+    time.sleep(5)
+    for i in range(3):
+       driver.find_element('css selector', 'div.product-free:nth-child(1)').click()
+       time.sleep(30)
+    print('finish')
 
-    driver.get('https://en.onlinesoccermanager.com/BusinessClub')
-    time.sleep(20)
-    for j in range (3):
-        driver.find_element("xpath",'/html/body/div[3]/div[4]/div/div/div[2]/div[2]/div/div[1]/div/div/div/div[2]').click()
-        time.sleep(70)
-
-
-user=input('Online Soccer Manager User: ')
-password=input('Online Soccer Manager Password: ')
-
+user=os.environ['USER']
+password=os.environ['PASSWORD']
+print(user,password)
 login(user,password)
-
+getBusinessTokens()
