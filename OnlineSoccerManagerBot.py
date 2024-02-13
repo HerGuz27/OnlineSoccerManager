55ef65c1325557f623d745868969be310c325e08
Hernan Gutierrez <hernangut96@gmail.com>
2024-02-13 20:16:33 -0300

    Add some logs

--- OnlineSoccerManagerBot.py
+++ OnlineSoccerManagerBot.py
@@ -7,19 +7,30 @@ driver = webdriver.Firefox(options=options)
 
 
 def login(user,password):
+
+    #Open 'https://en.onlinesoccermanager.com' in firefox
     driver.get('https://en.onlinesoccermanager.com')
     time.sleep(5)
+
+    #Click in accept button
     driver.find_element('css selector', '.btn-new').click()
     time.sleep(5)
+
+    #Click in the login button
     driver.find_element('css selector', '.btn-alternative').click()
     time.sleep(5)
+
+    #Enter the user name
     driver.find_element("css selector",'#manager-name').send_keys(user)
+
+    #Enter the password
     driver.find_element("css selector",'#password').send_keys(password)
+
+    #Click in the login button
     driver.find_element("css selector",'#login').click()
     time.sleep(15)
 
-def getBusinessTokens():
-    print('Negocios')
+def get_business_tokens():
     time.sleep(5)
     driver.find_element('css selector', 'li.dropdown:nth-child(3) > a:nth-child(1)').click()
     time.sleep(5)
@@ -30,8 +41,13 @@ def getBusinessTokens():
        time.sleep(30)
     print('finish')
 
+
+# User and Paswword set with env variables
 user=os.environ['USER']
 password=os.environ['PASSWORD']
-print(user,password)
+
+#Execute login function
 login(user,password)
-getBusinessTokens()
+
+#Execute get
+get_business_tokens()
